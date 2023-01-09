624297556aa3224a4173d632b3868da54b68f669
guilherme <guioliveirasaar@gmail.com>
2023-01-09 13:12:34 -0300

    Added "+" bt and AddMenu

---
+++ view/index.js
@@ -0,0 +1,25 @@
+const upbar = document.querySelector(".bar1");
+const middlebar = document.querySelector(".bar2");
+const downbar = document.querySelector(".bar3");
+const burguer = document.querySelector(".bars");
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+const addButton = document.querySelector(".add-button");
+const addMenu = document.querySelector(".add-menu");
+
+burguer.addEventListener("click", () => {
+  upbar.classList.toggle("upcross");
+  middlebar.classList.toggle("middlecross");
+  downbar.classList.toggle("downcross");
+  menu.classList.toggle("menuopen");
+});
+
+addButton.addEventListener("click", () => {
+  addMenu.classList.toggle("add-menuOpen");
+});
+
+document.addEventListener("click", function (event) {
+  if (event.target !== addMenu && event.target !== addButton) {
+    addMenu.classList.toggle("add-menuOpen");
+  }
+});
