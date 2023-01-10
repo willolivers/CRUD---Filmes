fee8487562380a567953c5026b744d08cef9c682
guilherme <guioliveirasaar@gmail.com>
2023-01-09 21:17:53 -0300

    Some data

--- view/index.js
+++ view/index.js
@@ -23,3 +23,24 @@ document.addEventListener("click", function (event) {
     addMenu.classList.replace("add-menuOpen", "add-menu");
   }
 });
+
+const filmes = [
+  {
+    titulo: "Thor: Amor e o Trovão",
+    lançamento: "2020",
+    elenco: "Chris Hemsworth, Tessa Thompson, Natalie Portman, Christian Bale",
+    gênero: "Ação",
+  },
+  {
+    titulo: "Encanto",
+    lançamento: "2021",
+    elenco: "Emily Blunt, Lin-Manuel Miranda, John Krasinski, Emily Mortimer",
+    gênero: "Comédia",
+  },
+  {
+    titulo: "Avatar: O Caminho da Água",
+    lançamento: "2022",
+    elenco: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    gênero: "Ficção científica",
+  },
+];
